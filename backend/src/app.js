63b48c626047e53443ed6e88b97db3cc043c1b8b
AkshaySakat <aakshaysakat@gmail.com>
2026-03-05 19:32:43 +0530

    Fixed path for UI

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,10 +19,10 @@ app.get('/health', (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
 /* Serve React Build */
-app.use(express.static(path.join(__dirname, '../../frontend/build')));
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.get((req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 }
 /* Error Middleware (ALWAYS LAST) */
