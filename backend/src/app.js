02b024baf6e8be23c91bb71c5b21bff312c0ed4d
AkshaySakat <aakshaysakat@gmail.com>
2026-03-05 19:41:44 +0530

    updated route path

--- backend/src/app.js
+++ backend/src/app.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 /* Serve React Build */
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
-app.get((req, res) => {
+app.get('*',(req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 }
