9acbcd42eae35803c9a3f0eb1022f47a741462ed
AkshaySakat <aakshaysakat@gmail.com>
2026-03-07 13:17:59 +0530

    Updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', ()=> {
-    console.log(`Server is runng on PORT ${PORT}`);
+    console.log(`Server is runng on PORT http://localhost:${PORT}`);
 });
