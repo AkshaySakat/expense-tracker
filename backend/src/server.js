96a03cc30c350783b047fd357a1ff4a358f80f3a
AkshaySakat <aakshaysakat@gmail.com>
2026-03-11 23:47:37 +0530

    Setup a parameter store and iam roles from env variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
+const loadEnv = require("./config/loadEnv");
 
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, '0.0.0.0', ()=> {
-    console.log(`Server is runng on PORT http://localhost:${PORT}`);
-});
+async function startServer() {
+    await loadEnv();
+    const app = require('./app');
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on ${process.env.PORT}`);
+  });
+};
+startServer();
