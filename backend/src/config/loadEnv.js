19859f00cd58a70aabdf621507983f2adb41b9ac
AkshaySakat <aakshaysakat@gmail.com>
2026-03-12 12:01:14 +0530

    added logs for debugging

--- backend/src/config/loadEnv.js
+++ backend/src/config/loadEnv.js
@@ -1,10 +1,12 @@
+console.log("_____________Welcome to loadEnv.js_________");
 const { SSMClient, GetParametersByPathCommand } = require("@aws-sdk/client-ssm");
 require('dotenv').config();
 const client = new SSMClient({ region: "ap-south-1" });
-console.log("Welcome to loadEnv.js ");
 
 async function loadEnv() {
-  const env = process.env.APP_ENV || "qa";
+  try {
+    const env = process.env.APP_ENV || "qa";
+  console.log("_____________env_________", env);
   // Skip AWS when running locally
   if (env === "local") {
     console.log("Running locally. Using .env variables.");
@@ -17,17 +19,23 @@ async function loadEnv() {
     Recursive: true,
     WithDecryption: true
   });
-
+console.log("_____________command_________", command);
   const response = await client.send(command);
 
   console.log('Load Config response :: ', response);
 
   response.Parameters.forEach(param => {
     const key = param.Name.split("/").pop();
+    console.log("_____________key_________", key);
+    console.log("_____________key_________", param.Value);
     process.env[key] = param.Value;
   });
 
   console.log("Environment variables loaded from Parameter Store");
+  } catch (error) {
+    throw error;
+  }
+  
 }
 
 module.exports = loadEnv;
